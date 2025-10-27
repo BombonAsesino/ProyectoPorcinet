7ff95360a8fc04507ae91f2c70348526ea9baac3
Jamir <yasmirteyez533@gmail.com>
2025-10-27 07:39:04 -0600

    Correccion

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,11 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: ['react-native-reanimated/plugin'],
+    plugins: [
+      // Puedes tener otros plugins aquí (si los usas)
+      
+      // ⬇️ Este SIEMPRE debe ir de último:
+      'react-native-worklets/plugin'
+    ],
   };
 };
